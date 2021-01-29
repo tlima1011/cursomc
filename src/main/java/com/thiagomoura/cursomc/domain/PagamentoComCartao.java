977d5f45f9b9bbb8f7c8f1b181742ed06cefbbab
Thiago <thiagolima1011@gmail.com>
2021-01-29 18:56:20 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/thiagomoura/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,23 @@
+package com.thiagomoura.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.thiagomoura.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	
+	private static final long serialVersionUID = 1L;
+	private Integer numeroDeParcelas; 
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido,Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas; 
+	}
+	
+	
+}
